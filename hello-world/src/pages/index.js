fb8fd5a0be8a4cb5410e8a188d8ecf1286023b50
randomforestwhitaker <ryan.tanada.0@gmail.com>
2019-01-01 23:16:33 -0800

    Add About and Contact pages, with links. Update dependencies.

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,8 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "..//components/header"
 
 export default () => (
     <div style={{ color: `purple`}}>
-      <h1>Hello Gatsby!</h1>
+      <Link to="/contact/">Contact</Link>
+      <Header headerText="Hello Gatsby!" />
       <p>What a world.</p>
       <img src="https://images2.minutemediacdn.com/image/upload/c_fill,w_912,h_516,f_auto,q_auto,g_auto/shape/cover/sport/fbl-eng-pr-arsenal-tottenham-5c065f433d3f02f033000004.jpg"></img>
     </div>
