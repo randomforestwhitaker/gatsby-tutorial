c561c8fc95994ca2a0e994a775f2a025f7d2b21f
randomforestwhitaker <ryan.tanada.0@gmail.com>
2019-01-01 22:26:07 -0800

    Add Torreira homage to homepage

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <div style={{ color: `purple`}}>
+      <h1>Hello Gatsby!</h1>
+      <p>What a world.</p>
+      <img src="https://images2.minutemediacdn.com/image/upload/c_fill,w_912,h_516,f_auto,q_auto,g_auto/shape/cover/sport/fbl-eng-pr-arsenal-tottenham-5c065f433d3f02f033000004.jpg"></img>
+    </div>
+)
